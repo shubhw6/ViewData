07e24e5baa58c10659df8ffb4a9ba809fa494f05
Shubh <shubhw6@gmail.com>
2023-09-21 12:53:59 +0530

    Add project files.

---
+++ ViewData/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewData.Models;
+
+namespace ViewData.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewData["Name"] = "Shubh Verma";
+            ViewBag.Name = "Shubh";
+            ViewData["Age"] = 12;
+
+            ViewData["NAmeList"]= new string[] { "Rajesh","Ajay","Amar"};
+
+
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Akasha",
+                Description = "Developer",
+               
+            };
+
+            ViewData["StudentList"] = student;
+
+            return View();
+        }
+    }
+}
---
+++ ViewData/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace ViewData.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
